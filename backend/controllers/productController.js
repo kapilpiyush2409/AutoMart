5aa963ea8592c7ba92aff651a4fbd8fc2f191949
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-05-21 16:24:42 +0530

    pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -18,9 +18,12 @@ exports.newProduct =catchAsyncError( async (req, res, next) => {
 // Get new product => /api/v1/product
 exports.getProducts = catchAsyncError(async(req,res,next) =>{
 
+    const resPerPage=4;
+
     const apiFeatures=new APIFeatures(Product.find(),req.query)
                         .search()
                         .filter()
+                        .pagination(resPerPage)
 
     const products = await apiFeatures.query;
     
--- backend/util/apiFeatures.js
+++ backend/util/apiFeatures.js
@@ -30,18 +30,28 @@ class APIFeatures{
 
         removeFields.forEach(el => delete queryCopy[el]);
 
-        console.log(queryCopy);
+        //console.log(queryCopy);
        // Advance filter for price ,ratimgd etc
 
        let queryStr=JSON.stringify(queryCopy)
         queryStr= queryStr.replace(/\b(gt|gte|lt|lte)\b/g,match => `$${match}`)
 
-        console.log(queryStr)
+       // console.log(queryStr)
 
        this.query=this.query.find(JSON.parse(queryStr));
         
        return this;
 
     }
+
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryStr.page)||1;
+        const skip=resPerPage * (currentPage -1);
+
+        this.query=this.query.limit(resPerPage).skip(skip);
+
+        return this;
+    }
+
 }
 module.exports = APIFeatures
