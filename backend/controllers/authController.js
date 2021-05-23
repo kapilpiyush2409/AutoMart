d1682e6885d0e41733d588caff23060884866194
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-05-23 23:27:10 +0530

    cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 
 const ErrorHandler=require('../util/errorHandler');
 const catchAsyncErrors= require('../middlewares/catchAsyncError');
+const sendToken = require('../util/jwtToken');
 
 // Register a user => api /v1/register
 
@@ -19,12 +20,7 @@ exports.registerUser = catchAsyncErrors(async(req, res, next) =>{
         }
     })
 
-    const token = user.getJwtToken()
-
-    res.status(201).json({
-        success : true,
-        token
-    })
+    sendToken(user,200,res)
 
 })
 //login user => /api/v1/login
@@ -53,9 +49,5 @@ exports.loginUser = catchAsyncErrors(async(req,res,next) =>{
     if(!isPasswordMatched){
         return next(new ErrorHandler('Invalid Email or Password',401));
     }
-    const token =user.getJwtToken();
-    res.status(200).json({
-        success: true,
-        token
-        })
+    sendToken(user,200,res)
 })
