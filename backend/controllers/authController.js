bd8c6ffa8e3dc639ae7500edc16f19d0cd2bb9b2
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-05-23 23:14:52 +0530

    login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,3 +27,35 @@ exports.registerUser = catchAsyncErrors(async(req, res, next) =>{
     })
 
 })
+//login user => /api/v1/login
+
+exports.loginUser = catchAsyncErrors(async(req,res,next) =>{
+    const {email,password} = req.body
+
+    //checks if email and password is enter by user
+
+    if(!email || !password) {
+        return next(new ErrorHandler('please enter your eamil & password',400))
+
+    }
+
+    // finding user in db
+
+    const user = await User.findOne({email}).select('+password')
+    
+    if(!user){
+        return next(new ErrorHandler('Invalid Email or Password',401));
+    }
+
+    // check if p is correct or not
+    const isPasswordMatched =await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler('Invalid Email or Password',401));
+    }
+    const token =user.getJwtToken();
+    res.status(200).json({
+        success: true,
+        token
+        })
+})
