cf9f960ecfc64e9f381d66b12e18177e508eaae0
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-05-24 17:37:52 +0530

    Send Password Recovery Email

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,8 @@ const User = require('../models/user')
 const ErrorHandler=require('../util/errorHandler');
 const catchAsyncErrors= require('../middlewares/catchAsyncError');
 const sendToken = require('../util/jwtToken');
+const sendEmail = require('../util/sendEmail');
+
 
 // Register a user => api /v1/register
 
@@ -51,6 +53,55 @@ exports.loginUser = catchAsyncErrors(async(req,res,next) =>{
     }
     sendToken(user,200,res)
 })
+// Forgot password => /api/v1/password/forgot
+
+exports.forgotPassword = catchAsyncErrors(async(req, res, next) =>{
+
+    const user = await User.findOne({ email: req.body.email });
+
+    if(!user){
+        return next(new ErrorHandler('User is not found with this image',404));
+    }
+
+    // Get Reset token
+
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({ validateBeforeSave: false})
+
+    //Create reset password url": "
+
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+
+    const message =`Your password reset token is as follows:\n\n${resetUrl}\n\n\If you have not
+    requested this email then ignore it.`
+
+    try {
+
+
+        await sendEmail({
+            email: user.email,
+            subject:'AutoMart Password Recovery',
+            message
+
+        })
+
+        res.status(200).json({
+            success: true,
+            message:`Email sent to: ${user.email}`
+        })
+
+
+    }catch(error){
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({ validateBeforeSave: false})
+
+        return next( new ErrorHandler(error.massage,500))
+    }
+
+}) 
 
 // logout user => /api/v1/logout
 
