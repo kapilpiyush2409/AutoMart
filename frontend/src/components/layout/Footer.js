9c971f956ff2c3b389b2bf173817bb809068b7dc
kapilpiyush2409 <kapilpiyush33@gmail.com>
2021-05-26 22:17:15 +0530

    Make a layout of hom page

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+
+import '../../App.css'
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <footer className="py-1">
+                <p className="text-center  mt-1">
+                    AutoMart -2021-2030,All Rights Reserved
+                </p>
+            </footer>
+        </Fragment>
+    )
+}
+
+export default Footer
